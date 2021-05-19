2c5fb48d010d982732ce7454f6fd1faf329b3e91
Amro Khaled | Commentatk <hi@amrography.com>
2021-05-19 11:18:06 +0200

    :zap: Can get categories, sub categories and children

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -3,6 +3,7 @@
 namespace Akhaled\Like4Card\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Akhaled\Like4Card\Database\Factories\CategoryFactory;
 
@@ -12,8 +13,18 @@ class Category extends Model
 
     protected $guarded = [];
 
+    public function scopeParents(Builder $builder)
+    {
+        $builder->whereNull('parent_id');
+    }
+
     protected static function newFactory()
     {
         return CategoryFactory::new();
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
     }
 }
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Akhaled\Like4Card\Tests\Unit\Models;
+
+use Akhaled\Like4Card\Models\Category;
+use Akhaled\Like4Card\Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function test_it_gets_parent_categories_only()
+    {
+        $parent_categories_count = 5;
+
+        Category::factory()
+            ->count($parent_categories_count)
+            ->create()
+            ->each(function (Category $category) {
+                Category::factory()
+                    ->count(2)
+                    ->create(['parent_id' => $category->id]);
+            });
+
+        $this->assertCount($parent_categories_count, Category::parents()->get());
+    }
+
+    public function test_it_gets_sub_categories()
+    {
+        $categories = Category::factory()
+            ->count(2)
+            ->create()
+            ->each(function (Category $category, int $index) {
+                Category::factory()
+                    ->count($index + 3)
+                    ->create(['parent_id' => $category->id]);
+            });
+
+        $this->assertCount(
+            3,
+            Category::find(
+                $categories->first()->id
+            )
+                ->children()
+                ->get()
+        );
+    }
+}
