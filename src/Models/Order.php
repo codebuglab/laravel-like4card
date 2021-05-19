692835cfb5de6b0d9b477ed6469b414fa8db1987
Amro Khaled | Commentatk <hi@amrography.com>
2021-05-19 10:16:14 +0200

    :zap: Init model and migrations

---
+++ src/Models/Order.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Akhaled\Like4Card\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
